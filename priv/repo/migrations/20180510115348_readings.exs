005b400119612a55ebc1f0d9ebec102a12e0de8f
skota <sriramkota@yahoo.com>
2018-05-10 08:22:59 -0400

    add detail to user and readings migration

--- priv/repo/migrations/20180510115348_readings.exs
+++ priv/repo/migrations/20180510115348_readings.exs
@@ -2,6 +2,10 @@ defmodule Sugarlogapp.Repo.Migrations.Readings do
   use Ecto.Migration
 
   def change do
-
+    add :user_id, :integer, null: false
+    add :reading, :ineteger, null: false
+    add :time_of_day, :string, null: false
+    add :reading_taken_dt, :utc_datetime, null: true
+    timestamps()
   end
 end
