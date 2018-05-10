b50c5567bde64abfea2eebcc1fa859486049f7b6
skota <sriramkota@yahoo.com>
2018-05-10 08:29:28 -0400

    fix typos in migrations

--- priv/repo/migrations/20180510115117_user.exs
+++ priv/repo/migrations/20180510115117_user.exs
@@ -2,19 +2,21 @@ defmodule Sugarlogapp.Repo.Migrations.User do
   use Ecto.Migration
 
   def change do
-    add :first_name, :string, null: false
-    add :last_name, :string, null: false
-    add :email, :string, null: false
-    add :deleted_at, :utc_datetime, null: true
-    add :remember_token, :string, null: true
-    add :date_of_birth, :naive_datetime, null: true
-    add :role, :string, size: 1, null: false
-    add :active, :boolean, default: 0, null: false    
-    add :password_hash, :string, null: false
-    add :activation_token, :string,  null: true
-    add :activation_token_created_at, :utc_datetime, null: true
-    add :password_reset_token, :string, null: true
-    add :password_reset_token_created_at, :utc_datetime, null: true
-    timestamps()
+    create table(:users) do
+      add :first_name, :string, null: false
+      add :last_name, :string, null: false
+      add :email, :string, null: false
+      add :deleted_at, :utc_datetime, null: true
+      add :remember_token, :string, null: true
+      add :date_of_birth, :naive_datetime, null: true
+      add :role, :string, size: 1, null: false
+      add :active, :boolean, default: false
+      add :password_hash, :string, null: false
+      add :activation_token, :string,  null: true
+      add :activation_token_created_at, :utc_datetime, null: true
+      add :password_reset_token, :string, null: true
+      add :password_reset_token_created_at, :utc_datetime, null: true
+      timestamps()
+    end  
   end
 end
--- priv/repo/migrations/20180510115348_readings.exs
+++ priv/repo/migrations/20180510115348_readings.exs
@@ -2,10 +2,12 @@ defmodule Sugarlogapp.Repo.Migrations.Readings do
   use Ecto.Migration
 
   def change do
-    add :user_id, :integer, null: false
-    add :reading, :ineteger, null: false
-    add :time_of_day, :string, null: false
-    add :reading_taken_dt, :utc_datetime, null: true
-    timestamps()
+    create table(:readings) do
+      add :user_id, :integer, null: false
+      add :reading, :integer, null: false
+      add :time_of_day, :string, null: false
+      add :reading_taken_dt, :utc_datetime, null: true
+      timestamps()
+    end  
   end
 end
