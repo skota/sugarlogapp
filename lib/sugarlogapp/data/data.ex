4f34a5e42b3c819dad0eaadc26779aa47f70561a
skota <sriramkota@yahoo.com>
2018-05-16 08:14:21 -0400

    add repo

---
+++ lib/sugarlogapp/data/data.ex
@@ -0,0 +1,19 @@
+defmodule SugarlogappWeb.Data do
+    import Ecto.Query, warn: false
+    alias SugarlogappWeb.Repo.User
+    # need password validator
+    import Comeonin.Bcrypt, only: [hashpwsalt: 1]
+
+
+    #add user schema
+
+    #define changesets
+
+
+    #ensure params passed to changeset have password
+
+
+
+    #change set for update
+
+end    
---
+++ lib/sugarlogapp/data/user.ex
@@ -0,0 +1,4 @@
+defmodule SugarlogappWeb.Data.User do
+    use Ecto.Schema
+    import Ecto.Changeset
+end
