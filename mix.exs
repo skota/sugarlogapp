22fc4de2087b6455c7a16e75af684cb816ed4128
sriram kota <sriramkota@yahoo.com>
2018-08-05 18:23:38 -0400

    updated phoenix version. Add mariax deps. Remove api folder

--- mix.exs
+++ mix.exs
@@ -33,10 +33,10 @@ defmodule Sugarlogapp.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.2"},
+      {:phoenix, "~> 1.3.3"},
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
+      {:mariaex, "~> 0.8.3"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
