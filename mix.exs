51e8c145edd28b256c2309fe877598b260aff77a
skota <sriramkota@yahoo.com>
2018-09-03 16:37:37 -0400

    add edeliver to application

--- mix.exs
+++ mix.exs
@@ -20,7 +20,7 @@ defmodule Sugarlogapp.Mixfile do
   def application do
     [
       mod: {Sugarlogapp.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools,  :edeliver]
     ]
   end
 
