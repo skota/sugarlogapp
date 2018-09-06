6a40b3129b2c83c9773d38bee124209c92a2ad97
skota <sriramkota@yahoo.com>
2018-09-06 09:15:50 -0400

    still trying

--- rel/commands/migrate.sh
+++ rel/commands/migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # release_ctl eval --mfa "MyApp.ReleaseTasks.migrate/1" -- "$@"
-release_ctl eval -mfa "Sugarlogapp.ReleaseTasks/migrate/0" -- ""
+release_ctl eval -mfa "Sugarlogapp.ReleaseTasks/migrate/0" -- "$@"
