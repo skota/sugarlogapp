181a15f12e87951b8c1094347226adf7447a31cd
skota <sriramkota@yahoo.com>
2018-09-07 14:40:02 -0400

    updated release_tasks with new function

--- rel/commands/migrate.sh
+++ rel/commands/migrate.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # release_ctl eval --mfa "MyApp.ReleaseTasks.migrate/1" -- "$@"
-release_ctl eval --mf "Sugarlogapp.ReleaseTasks" -- "$@"
+bin/sugarlogapp command Elixir.Sugarlogapp.ReleaseTasks migrate_db
